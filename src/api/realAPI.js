d59f75a32dab38a380b4600d0fcaa3fd45bff884
zayez <zayezuno@protonmail.com>
2023-01-22 20:08:42 -0300

    Swap placeholder api parameters for the real deal

--- src/api/realAPI.js
+++ src/api/realAPI.js
@@ -1,10 +1,14 @@
+import { format } from "date-fns"
 const baseUrl = `https://api.nasa.gov/planetary`
 import { STATUS_ERR, STATUS_OK } from "@/types/HttpStatus"
 import config from "../../config"
+import { dateSubtract } from "@/helpers/dateHelpers"
 const API_KEY = config.nasa.api_key
 
-const getPhotos = async ({ date }) => {
-  const dateRange = `start_date=2022-01-01&end_date=2022-01-20`
+const getPhotos = async (date, interval = 3) => {
+  const endDate = format(new Date(date), "yyyy-MM-dd")
+  const startDate = format(dateSubtract(new Date(date), interval), "yyyy-MM-dd")
+  const dateRange = `start_date=${startDate}&end_date=${endDate}`
   const url = `${baseUrl}/apod?api_key=${API_KEY}&${dateRange}`
   const res = await fetch(url)
   switch (res.status) {
