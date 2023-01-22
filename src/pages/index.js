1ef9d568e666c55f42a3a01255fdd72acbd3d663
zayez <zayezuno@protonmail.com>
2023-01-22 11:34:51 -0300

    Use PhotoGallery component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,31 @@
+import Loader from "@/comps/Loader"
+import PhotoGallery from "@/comps/PhotoGallery"
+import { fetchPhotos, selectPhotos } from "@/store/slices/photosSlice"
+import { SPINNER_TYPE } from "@/types/LoaderType"
 import Head from "next/head"
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 const Index = () => {
   const dispatch = useDispatch()
-  useEffect(() => {}, [])
+  const photos = useSelector(selectPhotos)
+  useEffect(() => {
+    dispatch(fetchPhotos(new Date()))
+  }, [])
   return (
     <>
       <Head>
         <title>Sideralis | Home </title>
       </Head>
-      <div>
+      <div className="container">
         <h1>Home</h1>
+        {!photos.loading && photos.photos?.length ? (
+          <PhotoGallery photos={photos.photos.slice(0, 8)} />
+        ) : null}
+
+        {/* <div className="loader-container">
+          <Loader type={SPINNER_TYPE} size="medium" />
+        </div> */}
       </div>
     </>
   )
