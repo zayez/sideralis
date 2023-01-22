8a572db7af5913fe2e0b0cfd0d1ec2ab7ff12204
zayez <zayezuno@protonmail.com>
2023-01-22 20:10:45 -0300

    Improve card component
    Add new sections (header, footer) to display more information about the
    picture.

--- src/comps/PhotoGallery.js
+++ src/comps/PhotoGallery.js
@@ -1,10 +1,20 @@
+import { format } from "date-fns"
 const PhotoItem = ({ date, title, explanation, url, hdurl }) => {
   return (
     <>
       <div className="card">
-        <a href="">
-          <img className="card-image" src={url} />
-        </a>
+        <div className="card-header">
+          <h3> {format(new Date(date), "dd/MM/yyyy")}</h3>
+        </div>
+        <div className="card-body">
+          <a href="#">
+            <img className="card-image" src={url} />
+          </a>
+        </div>
+        <div className="card-footer">
+          <h3>{title}</h3>
+          <p>{explanation.substring(0, 150)}...</p>
+        </div>
       </div>
     </>
   )
