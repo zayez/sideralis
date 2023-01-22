d5c9a3e6221a66e3bc72b49f0bc8fb9fb9c8d8e2
zayez <zayezuno@protonmail.com>
2023-01-22 20:09:58 -0300

    Add params to mockAPI

--- src/api/mockAPI.js
+++ src/api/mockAPI.js
@@ -1,7 +1,10 @@
+import { dateSubtract } from "@/helpers/dateHelpers"
 import { STATUS_OK } from "@/types/HttpStatus"
 import photosData from "../../tests/fixtures/photos.json"
 
-const getPhotos = async ({}) => {
+const getPhotos = async (date, interval = 3) => {
+  const d1 = new Date(date)
+  const d2 = dateSubtract(date, interval)
   return Promise.resolve({ status: STATUS_OK, payload: photosData })
   // return Promise.reject(
   //   new Error("Unable to retrieve photos for this date range.")
