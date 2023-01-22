800c7028db5ec95f5ce9e9dd53c78a811b6dfccb
zayez <zayezuno@protonmail.com>
2023-01-22 20:13:35 -0300

    Improve code for fetchPhotos, add fetchMorePhotos

--- src/store/slices/photosSlice.js
+++ src/store/slices/photosSlice.js
@@ -1,10 +1,14 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { format } from "date-fns"
 import { STATUS_ERR, STATUS_OK } from "../../types/HttpStatus"
 import api from "../../api"
+import { dateSubtract } from "@/helpers/dateHelpers"
 
 const initialState = {
   loading: false,
+  loadingMore: false,
   photos: [],
+  currentDate: null,
   error: "",
 }
 
@@ -14,14 +18,36 @@ const photosSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(fetchPhotos.pending, (state) => {
       state.loading = true
+      state.photos = []
     })
     builder.addCase(fetchPhotos.fulfilled, (state, action) => {
       state.loading = false
-      state.photos = action.payload
+      state.currentDate = new Date(action.payload.date).toISOString()
+      state.photos = [...action.payload.data.reverse()]
       state.error = ""
     })
     builder.addCase(fetchPhotos.rejected, (state, action) => {
       state.loading = false
+      const { status, message } = action.payload
+      switch (status) {
+        case STATUS_ERR:
+        default:
+          state.error = message
+      }
+    })
+
+    builder.addCase(fetchMorePhotos.pending, (state) => {
+      state.loadingMore = true
+    })
+    builder.addCase(fetchMorePhotos.fulfilled, (state, action) => {
+      state.loadingMore = false
+      state.currentDate = new Date(action.payload.date).toISOString()
+
+      state.photos = [...state.photos, ...action.payload.data.reverse()]
+      state.error = ""
+    })
+    builder.addCase(fetchMorePhotos.rejected, (state, action) => {
+      state.loadingMore = false
       const { status, message } = action.payload
       switch (status) {
         case STATUS_ERR:
@@ -34,12 +60,29 @@ const photosSlice = createSlice({
 
 export const fetchPhotos = createAsyncThunk(
   "photos/fetchPhotos",
-  async (date, { rejectWithValue }) => {
+  async ({ date, interval = 7 }, { rejectWithValue }) => {
     try {
-      const { status, payload } = await api.getPhotos(date)
+      const { status, payload } = await api.getPhotos(date, interval)
       switch (status) {
         case STATUS_OK:
-          return payload
+          return { date, data: payload }
+        default:
+          return rejectWithValue(STATUS_ERR)
+      }
+    } catch (err) {
+      return rejectWithValue({ status: STATUS_ERR, message: err.message })
+    }
+  }
+)
+
+export const fetchMorePhotos = createAsyncThunk(
+  "photos/fetchMorePhotos",
+  async ({ date, interval = 7 }, { rejectWithValue }) => {
+    try {
+      const { status, payload } = await api.getPhotos(date, interval)
+      switch (status) {
+        case STATUS_OK:
+          return { date, data: payload }
         default:
           return rejectWithValue(STATUS_ERR)
       }
