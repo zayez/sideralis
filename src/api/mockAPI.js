35f8b0ce6d2c85634d692826732aee5766105d35
zayez <zayezuno@protonmail.com>
2023-01-27 17:26:18 -0300

    Add delay to mock api call

--- src/api/mockAPI.js
+++ src/api/mockAPI.js
@@ -2,14 +2,22 @@ import { dateSubtract } from "../helpers/dateHelpers"
 import { STATUS_OK } from "../types/HttpStatus"
 import photosData from "../../tests/fixtures/photos.json"
 
+import config from "../config"
+const API_RESPONSE_DELAY = config.app.api_response_delay
+
 const getPhotos = async (date, interval = 3) => {
   const d1 = new Date(date)
   const d2 = dateSubtract(date, interval)
-  return Promise.resolve({ status: STATUS_OK, payload: photosData })
-  // return Promise.reject(
-  //   new Error("Unable to retrieve photos for this date range.")
-  // )
+
+  let p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ status: STATUS_OK, payload: photosData })
+    }, API_RESPONSE_DELAY)
+  })
+
+  return p
 }
+
 const mockAPI = { getPhotos }
 
 export default mockAPI
--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,13 @@ export const nasa = {
   api_key: process.env.NEXT_PUBLIC_NASA_API_KEY,
 }
 
+export const app = {
+  api_response_delay: process.env.NEXT_PUBLIC_API_RESPONSE_DELAY,
+}
+
 const config = {
   nasa,
+  app,
 }
 
 export default config
