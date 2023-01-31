c907b021a0cf7f21fe321f35da2c24ee9db972e3
zayez <zayezuno@protonmail.com>
2023-01-31 15:33:41 -0300

    Edit about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,28 @@
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="container">
+      <h1>Sideralis</h1>
+      <h2>Overview</h2>
+      <blockquote>
+        <p>
+          "Astronomy Picture of the Day (APOD) is a website provided by NASA.
+          Each day a different image or photograph of our universe is featured,
+          along with a brief explanation written by a professional astronomer."
+        </p>
+
+        <a href="https://en.wikipedia.org/wiki/Astronomy_Picture_of_the_Day">
+          Wikipedia
+        </a>
+      </blockquote>
+      <p>
+        Sideralis is an open source APOD client. Read more on{" "}
+        <a href="https://github.com/zayez/sideralis">Github</a>.
+      </p>
+      <h2>License</h2>
+      <p>
+        Sideralis is distributed under the terms and conditions of the MIT
+        license.
+      </p>
     </div>
   )
 }
